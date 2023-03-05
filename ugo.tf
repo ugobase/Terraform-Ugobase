0e9525a905e07488d01cb758ec72a24189c22b06
Ugochukwu Mgboji <120265161+ugobase@users.noreply.github.com>
2023-03-05 10:56:25 +0100

    Add files via upload

---
+++ ugo.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "Ugobase"
+}
